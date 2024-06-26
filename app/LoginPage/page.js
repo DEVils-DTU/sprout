61d87d13b7fbebf1ed24da9630a4280e4b99c326
jainbhavit2018 <jainbhavit2018@gmail.com>
2024-06-26 13:27:33 +0530

    feat:login page, createposting page skeleton

--- app/LoginPage/page.js
+++ app/LoginPage/page.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const Login = () => {
+const LoginPage = () => {
     return (
-        <div className='text-black'>Login</div>
+        <div className='h-full w-full p-4 text-black'>
+            <form className='flex flex-col'>
+                <label>Hive Username</label>
+                <input type='text' required />
+                <label>Display Name</label>
+                <input type='text' required />
+                <button type='submit' className='bg-black text-white'>Submit</button>
+            </form>
+
+
+        </div>
     )
 }
 
-export default Login
+export default LoginPage
