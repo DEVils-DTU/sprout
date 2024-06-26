e13b574cff437de4b44d1a97ecf66d3be4c690af
jainbhavit2018 <jainbhavit2018@gmail.com>
2024-06-26 20:35:50 +0530

    chore: designs

--- app/ProjectView/[projectid]/page.js
+++ app/ProjectView/[projectid]/page.js
@@ -39,38 +39,34 @@ const ProjectView = ({ params }) => {
       <img src={postingData.coverImageURL} className="w-2/5" />
       <div className="text-black w-3/5 h-full font-poppins p-5 flex flex-col justify-start gap-5">
 
-        <h1 className=" font-extrabold text-[3vw] underline">
-          {postingData.title}
-        </h1>
-        <h2 className=" text-[1vw] italic">
-          By {postingData.authorDisplay} <br />
-        </h2>
+        <div className="flex flex-col gap-0">
+          <h1 className=" font-extrabold text-[3vw] underline">
+            {postingData.title}
+          </h1>
+          <h2 className=" text-[1vw] italic">
+            By {postingData.authorDisplay} <br />
+          </h2>
+        </div>
 
-        <div className="flex text-justify justify-evenly w-full">
-          <p className="py-5">
-            {postingData.text} <br />{" "}
-          </p>
+
+        <div className="flex text-justify justify-evenly w-[90%]">
+
+          {postingData.text} <br />{" "}
         </div>
 
         {loggedIn ? (
           username != postingData.author ? (
             // true ? (
             <div className="flex flex-col gap-2 w-full h-full items-start justify-start">
-              <h2 className="font-bold text-2xl">Place a Quotation</h2>
-              <input
-                type="number"
-                className="w-1/6 border-2 border-green-700 rounded-lg outline-none p-1"
-                placeholder="10000"
-              />
-              <button className="w-fit">Submit</button>
-              <p>kjvbjkdbncjfhvjkfhvjfvjfdv <br />
-                kjvbjkdbncjfhvjkfhvjfvjfdv <br />
-                kjvbjkdbncjfhvjkfhvjfvjfdv <br />
-                kjvbjkdbncjfhvjkfhvjfvjfdv <br />
-                kjvbjkdbncjfhvjkfhvjfvjfdv<br />
-                kjvbjkdbncjfhvjkfhvjfvjfdv<br />
-                kjvbjkdbncjfhvjkfhvjfvjfdv
-              </p>
+              <h2 className=" text-xl font-bold  drop-shadow-2xl rounded-xl">Place a Quotation</h2>
+              <div className="flex gap-3 w-full">
+                <input
+                  type="text"
+                  className="w-1/6 border-b-2 outline-none p-1"
+                  placeholder="10000"
+                />
+                <button className="text-[90%] font-lato font-bold p-2 border-2 border-green-900 bg-green-50 w-fit rounded-lg">Submit</button>
+              </div>
             </div>
           ) : (
             offers.map((offer) => (
